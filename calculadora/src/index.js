d134b2b3c85e15dd1bbd8adf453e2dea824139f0
LucasMatheus96 <lucasmatheus.martinss@gmail.com>
2025-03-26 23:29:06 -0300

    Criação dos botões da calculadora

--- calculadora/src/index.js
+++ calculadora/src/index.js
@@ -7,12 +7,10 @@ import Calculator from './main/Calculator';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- <Calculator></Calculator> 
+  <div>
+    <h1> Calculadora</h1>
+    <Calculator></Calculator> 
+  </div>
+
 );
-
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
