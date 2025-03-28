97ceb334bbfbfca27281b7d4319bee1950c9d43f
LucasMatheus96 <lucasmatheus.martinss@gmail.com>
2025-03-27 23:59:22 -0300

    Criando a logica dos buttons

--- calculadora/src/components/Button.jsx
+++ calculadora/src/components/Button.jsx
@@ -1,5 +1,31 @@
 import React from "react";
 import './Button.css'
 
-export default props => 
-    <button className="button">{props.label}</button>
+
+export default props=>{
+
+    // Declaro o valor padrão do className = button, e em seguida ele vai validar as propriedades que foram passadas ao componente para decidir quais estilos serão aplicados
+    let classes = 'button ';
+    classes += props.operation ? 'operation' : '';
+    classes += props.double ? 'double' : '';
+    classes += props.triple ? 'triple' : ''; 
+
+    return(
+    <button 
+    onClick={e=>props.click && props.click(props.label)}
+    className={classes}>
+        {props.label}
+
+    </button>)
+}
+
+// Forma direta, passando javascript puro dentro do corpo do class name usando `` e fazendo as validações das propriedades passadas
+// export default props => 
+//     <button className={`
+//         button
+//         ${props.operation ? 'operation' : ''}
+//         ${props.double ? 'double': ''}
+//         ${props.triple ? 'triple': ''}
+//         `}>
+//         {props.label}
+//     </button>
